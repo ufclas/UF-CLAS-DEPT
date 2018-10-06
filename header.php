2a276d93077eb9da1766a58d074b8861e6ee5f41
Singh <yash5@ls-cns-web05.clas.ufl.edu>
2018-10-05 23:24:53 -0400

    branding edits

--- header.php
+++ header.php
@@ -2223,7 +2223,7 @@ figcaption{
    .strikeheader {
      font-family: Quadon_Medium;
      font-weight:500;
-     font-size:2rem;
+     font-size:1.8rem;
      color:rgba(0, 64, 131, 1) !important;
      display: table-cell;
      height:3.125rem;
