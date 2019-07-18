c94eb1dd7c51bfad4ef9945148bc3710aba6ac5d
Vasquez <vp811@ufl.edu>
2019-07-18 14:15:52 -0400

    New template for publications site no featured image post

---
+++ single-no-feature-image.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * Template Name: No Featured Image
+ * Template Post Type: post, page, product
+ */
+
+get_header(); ?>
+
+<div class="wrap">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+			<?php
+			/* Start the Loop */
+			while ( have_posts() ) : the_post();
+
+				get_template_part( 'template-parts/post/content-right-sidebar', get_post_format() );
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+
+			endwhile; // End of the loop.
+			?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+	<?php get_sidebar(); ?>
+</div><!-- .wrap -->
+
+<?php get_footer();
