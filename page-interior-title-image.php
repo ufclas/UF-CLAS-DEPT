90a68971ed7a877dd78fb53c6538bf6874b78819
Vasquez <vp811@ufl.edu>
2019-07-24 15:22:23 -0400

    Creates new template to match the CLAS interior pages

---
+++ page-interior-title-image.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * Template Name: Interior Title Image
+ * Template Post Type: page
+ */
+
+get_header(); ?>
+
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+			<?php
+			/* Start the Loop */
+			while ( have_posts() ) : the_post();
+
+				get_template_part( 'template-parts/page/content-title-image', get_post_format() );
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+
+			endwhile; // End of the loop.
+			?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+
+
+<?php get_footer();
