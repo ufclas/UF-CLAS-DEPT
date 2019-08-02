5e07972528b61c21d0d7a804b15712b09817c541
Vasquez <vp811@ufl.edu>
2019-08-02 08:41:36 -0400

    Remove site ID from the header that was caused errors on production site

--- header.php
+++ header.php
@@ -35,7 +35,7 @@
 		$blog_id = get_current_blog_id();
 
 		//These are the site IDs for the CLAS website. 222 = Test server, 232 = Production server.
-		$clasID = array(222, 232, 247);
+		$clasID = array(232, 247);
 
 		//232 is the ID for the CLAS Home Page - Efren Vasquez
 		if( in_array($blog_id, $clasID) ){
