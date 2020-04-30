45185ac329bb7fa82a1205431e561534ea7c4f48
Vasquez <vp811@ufl.edu>
2020-04-30 14:29:14 -0400

    Fix structure of graduation page that was causing errors

--- page-recognition.php
+++ page-recognition.php
@@ -3,7 +3,29 @@
  * Template Name: Spring 2020 Recognition
  * Template Post Type: page
  */
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js no-svg">
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta property="og:title" content="<?php echo get_the_title();?>" />
+	<meta property="og:image" content="<?php echo get_the_post_thumbnail_url(get_the_ID(),'square-crop'); ?>" />
+	<meta property="og:image:type" content="image/jpeg" />
+	<meta property="og:image:width" content="500" />
+	<meta property="og:image:height" content="400" />
+	<link rel="profile" href="http://gmpg.org/xfn/11">
 
+	<?php wp_head(); ?>
+
+	<link rel="icon" href="<?php echo get_stylesheet_directory_uri().'/assets/images/favicon/favicon.ico' ?>" >
+	<link rel="apple-touch-icon" href="<?php echo get_stylesheet_directory_uri().'/assets/images/favicon/favicon-180.png' ?>">
+	<link rel="stylesheet" type="text/css" href="https://cloud.typography.com/6019574/7283992/css/fonts.css" />
+	<meta name="msapplication-TileColor" content="#00529b">
+	<meta name="msapplication-TileImage" content="<?php echo get_stylesheet_directory_uri().'/assets/images/favicon/favicon-144.png' ?>">
+</head>
+
+<?php
  $date_theDay = false;
 $majors = array(
 	"Doctoral Degrees" => array(
@@ -193,37 +215,21 @@ $majors = array(
   ),
 );
 ?>
-<html <?php language_attributes(); ?> class="no-js no-svg">
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta property="og:title" content="<?php echo get_the_title();?>" />
-	<meta property="og:image" content="<?php echo get_the_post_thumbnail_url(get_the_ID(),'square-crop'); ?>" />
-	<meta property="og:image:type" content="image/jpeg" />
-	<meta property="og:image:width" content="500" />
-	<meta property="og:image:height" content="400" />
-	<link rel="profile" href="http://gmpg.org/xfn/11">
-
-	<?php wp_head(); ?>
-
-	<link rel="icon" href="<?php echo get_stylesheet_directory_uri().'/assets/images/favicon/favicon.ico' ?>" >
-	<link rel="apple-touch-icon" href="<?php echo get_stylesheet_directory_uri().'/assets/images/favicon/favicon-180.png' ?>">
-	<link rel="stylesheet" type="text/css" href="https://cloud.typography.com/6019574/7283992/css/fonts.css" />
-	<meta name="msapplication-TileColor" content="#00529b">
-	<meta name="msapplication-TileImage" content="<?php echo get_stylesheet_directory_uri().'/assets/images/favicon/favicon-144.png' ?>">
-</head>
 
 <body <?php body_class(); ?>>
-  <img class="background" src="<?php echo get_stylesheet_directory_uri().'/images/recognition-ceremony/century-tower-background.jpg'?>" />
+  <img class="background" src="<?php echo get_stylesheet_directory_uri().'/images/recognition-ceremony/century-tower-background.jpg'?>" alt="Century Tower background image with blue overlay" />
   <div class="link-landing" id="top"></div>
 	<header>
     <div class="fixed-header">
       <div class="top-mobile-header">
         <div class="uf-wordmark">
          <?php get_template_part('images/recognition-ceremony/uf', 'wordmark.svg'); ?>
+         <span class="SVGaltText">University of Florida</span>
         </div>
-       <h1 class="mobile"><?php get_template_part('images/recognition-ceremony/uf', 'clas-wordmark-mobile.svg'); ?></h1>
-       <h1 class="tablet">College of Liberal Arts and Sciences</h1>
+       <h1>
+         <span class="mobile"><?php get_template_part('images/recognition-ceremony/uf', 'clas-wordmark-mobile.svg'); ?><span class="SVGaltText">College of Liberal Arts and Sciences</span></span>
+         <span class="tablet">College of Liberal Arts and Sciences</span>
+       </h1>
       </div>
     </div>
 
@@ -239,12 +245,13 @@ $majors = array(
 
 		    <div id="menu">
           <div class="top-hamburger-menu">
-  <div class="uf-wordmark">
-   <?php get_template_part('images/recognition-ceremony/uf', 'wordmark.svg'); ?>
-  </div>
- <h1 class="mobile"><?php get_template_part('images/recognition-ceremony/uf', 'clas-wordmark-mobile.svg'); ?></h1>
- <h1 class="tablet">College of Liberal Arts and Sciences</h1>
-</div>
+            <div class="uf-wordmark">
+             <?php get_template_part('images/recognition-ceremony/uf', 'wordmark.svg'); ?>
+             <span class="SVGaltText">University of Florida</span>
+            </div>
+           <span class="mobile"><?php get_template_part('images/recognition-ceremony/uf', 'clas-wordmark-mobile.svg'); ?><span class="SVGaltText">College of Liberal Arts and Sciences</span></span>
+           <span class="tablet">College of Liberal Arts and Sciences</span>
+          </div>
 					<nav role="navigation">
 							<ul>
 								<li><a href="#top">Home</a></li>
@@ -278,6 +285,7 @@ $majors = array(
   <main>
     <section id="container_graph_headers">
       <div class="inner-container">
+        <h2 class="visuallyhidden">Introduction</h2>
         <p class="graph_header">University of Florida</p>
         <p id="graph_header_titleSlug">College <span>of</span> Liberal Arts <span>and</span> Sciences</p>
         <p class="graph_header">Virtual Graduation Celebration</p>
@@ -301,7 +309,7 @@ $majors = array(
 		<section class="associate-deans-message section-container">
       <div class="inner-container">
   			<!-- <h3>Dean's Message</h3> -->
-  			<h3 class="congratulations">WELCOME MESSSAGE</h3>
+  			<h2 class="congratulations">WELCOME MESSSAGE</h2>
   			<p class="from-message">from Associate Dean Joe Spillane</p>
   			<iframe src="https://www.youtube.com/embed/cNr3r3nT8KE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" title="Joe Spillane Video" allowfullscreen></iframe>
 
@@ -317,12 +325,12 @@ $majors = array(
     <section class="student-message section-container">
       <div class="inner-container">
         <!-- <h3>Student Message</h3> -->
-        <h3 class="congratulations">CONGRATULATIONS</h3>
+        <h2 class="congratulations">CONGRATULATIONS</h2>
         <p class="from-message">from Student #1</p>
         <iframe src="https://www.youtube.com/embed/HY-wq_KCYm4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" title="Student Messages Video" allowfullscreen></iframe>
 
         <!-- <h3>Student Message</h3> -->
-        <h3 class="congratulations">CONGRATULATIONS</h3>
+        <h2 class="congratulations">CONGRATULATIONS</h2>
         <p class="from-message">from Student #2</p>
         <iframe src="https://www.youtube.com/embed/Wv4xATJv7pM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" title="Student Messages Video" allowfullscreen></iframe>
         <!--  Flower Icon -->
@@ -337,7 +345,7 @@ $majors = array(
     <section class="alumna-message section-container">
       <div class="inner-container">
         <!-- <h3>Associate Dean's Message</h3> -->
-        <h3 class="congratulations">CONGRATULATIONS</h3>
+        <h2 class="congratulations">CONGRATULATIONS</h2>
         <p class="from-message">from Domonique Worship</p>
         <iframe src="https://www.youtube.com/embed/PTASGJMJKmY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" title="Alumna Message Video" allowfullscreen></iframe>
 
@@ -353,7 +361,7 @@ $majors = array(
 		<section class="deans-message section-container">
       <div class="inner-container">
   			<!-- <h3>Dean's Message</h3> -->
-  			<h3 class="congratulations">CONGRATULATIONS</h3>
+  			<h2 class="congratulations">CONGRATULATIONS</h2>
   			<p class="from-message">from Dean David Richardson</p>
   			<iframe src="https://www.youtube.com/embed/lUhC-kvWN1s" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" title="Dean's Message Video" allowfullscreen></iframe>
 
@@ -369,7 +377,7 @@ $majors = array(
 		<section class="major-recognition section-container">
       <div class="inner-container">
   			<!-- <h3>Associate Dean's Message</h3> -->
-  			<h3 class="congratulations">CONGRATULATIONS</h3>
+  			<h2 class="congratulations">CONGRATULATIONS</h2>
   			<p class="from-message">from College Majors</p>
 
   			<div class="majors-container">
@@ -381,6 +389,7 @@ $majors = array(
 		<!-- 3. Student Highlights -->
     <div class="link-landing" id="social-media"></div>
   		<section class="social-media section-container">
+        <h2 class="visuallyhidden">Social Media</h2>
         <div class="inner-container">
           <div class="instagram-intro">
             <?php get_template_part('images/recognition-ceremony/uf', 'instagram-logo.svg'); ?>
