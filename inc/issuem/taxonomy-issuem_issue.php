b6f3fe6054198b70b6c1c89a337e941edd506001
Vasquez <vp811@ufl.edu>
2020-08-31 16:38:45 -0400

    Add issue description to issue page

--- inc/issuem/taxonomy-issuem_issue.php
+++ inc/issuem/taxonomy-issuem_issue.php
@@ -13,6 +13,12 @@ get_header(); ?>
 <div class="row">
   <div class="col-sm-12">
     <?php
+
+
+    //Display term description if there is one
+    echo term_description();
+
+
 		// Change the query if this is an issue page
 		if ( issuem_is_articles_page() ){
 			$issue_args = array(
@@ -47,19 +53,15 @@ get_header(); ?>
 		}
 	?>
 
-    <article id="issuem-article-<?php the_ID(); ?>" class="issuem-article col-lg-4 col-md-6" aria-label="<?php echo get_the_title(); ?>">
-    <div class="big-stat-wrap big-stat-img four gradient-bg"<?php echo $article_background; ?>>
-    <div class="big-stat-copy">
-        <h2><?php the_title( sprintf( '<a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a>' ); ?></h2>
-    </div>
-    <?php
-        if( has_term('', 'issuem_issue_categories') ){
-            $article_terms = get_the_terms( get_the_ID(), 'issuem_issue_categories' );
-            echo '<a href="' . get_term_link( $article_terms[0]->term_id, 'issuem_issue_categories' ) . '" class="category-tag">' . $article_terms[0]->name . '</a>';
-        }
-    ?>
-    </div>
-    </article>
+  <article id="issuem-article-<?php the_ID(); ?>" class="issuem-article col-lg-4 col-md-6" aria-label="<?php echo get_the_title(); ?>">
+    <a href="<?php echo get_permalink(); ?>">
+      <div class="big-stat-wrap big-stat-img four gradient-bg"<?php echo $article_background; ?>>
+        <div class="big-stat-copy">
+            <h2 style="color:white; font-family: Quadon_Regular; font-size: 1.4rem; letter-spacing: 1px;"><?php the_title( ); ?></h2>
+        </div>
+      </div>
+    </a>
+  </article>
     <?php endwhile; ?>
   </div>
 </div>
