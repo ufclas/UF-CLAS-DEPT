15cebac8a5fca0c5708104fad12b1635d53cd927
Efren Vasquez <e.vasquez@ufl.edu>
2021-02-22 15:39:05 -0500

    Remove accessibility error showing up from the i tag.

--- inc/custom-functions.php
+++ inc/custom-functions.php
@@ -560,7 +560,7 @@ add_filter( 'map_meta_cap', 'clasUnfilteredHtmlCapabilityToAdmins', 1, 3 );
 function ufclas_get_breadcrumb() {
   echo "<style type='text/css'> .entry-header .wrap {padding-top: 0;}</style>";
 
-  $delimiter = '<i class="fad fa-angle-double-right"></i>';
+  $delimiter = '<i class="fad fa-angle-double-right" aria-hidden="true"></i>';
   $name = 'Home'; //text for the 'Home' link
   $currentBefore = '<span class="current">';
   $currentAfter = '</span>';
