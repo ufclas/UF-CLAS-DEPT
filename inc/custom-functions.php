291c9a98112757fbb98cd61495506b7886a04237
Efren Vasquez <e.vasquez@ufl.edu>
2021-02-22 15:27:44 -0500

    Tribe events list layout update

--- inc/custom-functions.php
+++ inc/custom-functions.php
@@ -658,4 +658,24 @@ function ufclas_get_breadcrumb() {
 
   }
 }
+
+/**
+* Event List Widget: Add the month and day before the title
+*/
+function ufclas_events_widget_title(){
+ global $post;
+
+ // Change format of the start date for styling as an icon
+ $tribe_post_month = tribe_get_start_date( $post, false, 'M' );
+ $tribe_post_day = tribe_get_start_date( $post, false, 'd' );
+ ?>
+
+   <div class="event-date">
+     <span class="event-month"><?php echo $tribe_post_month; ?></span>
+       <span class="event-day"><?php echo $tribe_post_day; ?></span>
+   </div>
+
+   <?php
+}
+add_action( 'tribe_events_list_widget_before_the_event_title', 'ufclas_events_widget_title' );
 ?>
