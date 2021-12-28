fa36910114a261b8831df27af592efce2352f5d8
suisrael <s.israel@ufl.edu>
2021-12-27 20:09:39 -0500

    adding University of Florida; adjusting styles

--- template-parts/header/department.php
+++ template-parts/header/department.php
@@ -9,20 +9,53 @@
        </div>
 
 		   <div class="college subsite"><!-- College of Liberal Arts logo -->
-				<h1 class="strikeheader"><a class="strikeheader" href="<?php echo get_site_url(); ?>"><span class="access-focus"><?php echo  get_bloginfo( 'name' ) ?></span></a></h1>
-				 <div class="strikeRight">
-	 				<a href="https://clas.ufl.edu" target="_blank">
-	 						<span class="college-logo-desktop">
-	 							<?php echo twentyseventeen_get_svg( array( 'icon_id' => 'clas-connected' ) ); ?>
-	 							<span class="SVGaltText">College of Liberal Arts and Sciences</span>
-	 						</span>
-	 						<span class="college-logo-mobile">
-	 							<span>Liberal Arts and Sciences</span>
-	 					 </span>
-	 				</a>
-	 	   	</div>
-				<div class="menuBox">
-					<button><?php echo twentyseventeen_get_svg( array( 'icon_id' => 'icon-fivelines', 'class' => 'icon-menu', 'id' => 'icon-menu' ) ); ?></button>
+				 <div class="X-college-left">
+					 <h1 class="strikeheader"><a class="strikeheader" href="<?php echo get_site_url(); ?>"><span class="access-focus"><?php echo  get_bloginfo( 'name' ) ?></span></a></h1>
+					 <div class="strikeRight">
+		 				<a href="https://clas.ufl.edu" target="_blank">
+		 						<span class="college-logo-desktop">
+		 							<?php echo twentyseventeen_get_svg( array( 'icon_id' => 'clas-connected' ) ); ?>
+		 							<span class="SVGaltText">College of Liberal Arts and Sciences</span>
+		 						</span>
+		 						<span class="college-logo-mobile">
+		 							<span>Liberal Arts and Sciences</span>
+		 					 </span>
+		 				</a>
+		 	   	 </div>
+				 </div>
+				<div class="X-college-right">
+					<div class="university">
+						<a href="https://www.ufl.edu/" target="_blank">University of Florida</a>
+					</div>
+					<div class="donation desktop">
+					<?php
+					  if ( !empty(get_theme_mod('make_gift')) ){
+							 $donationURL = wp_kses_post( get_theme_mod('make_gift') );
+					     echo "<a href='$donationURL' target='_blank'>Make a Gift<span class='gift-icon'><em class='fas fa-gift'></em></a>";
+						 }
+					?>
+					</div>
+
+					<nav class="X-quickLinks" aria-label="Quick Links"><!--Quicklinks menu -->
+					  <a href="#" class="menu-title">Quick Links</a>
+						<?php wp_nav_menu( array(
+							'theme_location' => 'quicklinks',
+							'menu_id'        => 'quicklinks',
+					                'fallback_cb' => 'ufclas_emily_primary_menu_fallback',
+					                'depth' => 1,
+
+						) ); ?>
+			  	</nav>
+
+				<div class="X-searchTrigger"> <!-- Search section -->
+				   <button>
+					   <?php echo twentyseventeen_get_svg( array( 'icon_id' => 'icon-search-thin-open', 'class' => 'icon-search-thin-open', 'id' => 'icon-search-thin-trigger', 'title' => 'Open/Close Search' ) ); ?>
+				   </button>
+				</div>
+			   <div class="X-search-form-container"><?php get_search_form(); ?></div>
+			 				<div class="menuBox">
+						<button><?php echo twentyseventeen_get_svg( array( 'icon_id' => 'icon-fivelines', 'class' => 'icon-menu', 'id' => 'icon-menu' ) ); ?></button>
+					</div>
 				</div>
 		   </div>
 
@@ -60,32 +93,6 @@
 				 </div>
 		 </div>
    </div>
-	 	<div class="donation desktop">
-			<?php
-			  if ( !empty(get_theme_mod('make_gift')) ){
-					 $donationURL = wp_kses_post( get_theme_mod('make_gift') );
-			     echo "<a href='$donationURL' target='_blank'>Make a Gift<span class='gift-icon'><em class='fas fa-gift'></em></a>";
-				 }
-			?>
-		</div>
-
-		<nav class="X-quickLinks" aria-label="Quick Links"><!--Quicklinks menu -->
-		  <a href="#" class="menu-title">Quick Links</a>
-			<?php wp_nav_menu( array(
-				'theme_location' => 'quicklinks',
-				'menu_id'        => 'quicklinks',
-		                'fallback_cb' => 'ufclas_emily_primary_menu_fallback',
-		                'depth' => 1,
-
-			) ); ?>
-  	</nav>
-
-	<div class="X-searchTrigger"> <!-- Search section -->
-	   <button>
-		   <?php echo twentyseventeen_get_svg( array( 'icon_id' => 'icon-search-thin-open', 'class' => 'icon-search-thin-open', 'id' => 'icon-search-thin-trigger', 'title' => 'Open/Close Search' ) ); ?>
-	   </button>
-	</div>
-   <div class="X-search-form-container"><?php get_search_form(); ?></div>
- </div>
+</div>
 
 </header><!-- uf-branding -->
