6cb784b90e7d6b8929a5a29218a39a7ddb6b98c9
suisrael <s.israel@ufl.edu>
2023-06-06 11:50:36 -0400

    clear cache for social images

--- header.php
+++ header.php
@@ -18,12 +18,12 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta property="og:title" content="<?php echo get_the_title();?>" />
-	<meta property="og:image" content="<?php echo get_the_post_thumbnail_url(get_the_ID(),'square-crop'); ?>" />
+	<meta property="og:image" content="<?php echo get_the_post_thumbnail_url(get_the_ID(),'square-crop'); ?>?nocache=true" />
 	<meta property="og:image:type" content="image/jpeg" />
 	<meta property="og:image:width" content="500" />
 	<meta property="og:image:height" content="400" />
 	<meta property='twitter:title' content="<?php echo get_the_title();?>"/>
-	<meta property='twitter:image' content="<?php echo get_the_post_thumbnail_url(get_the_ID(),'square-crop'); ?>"/>
+	<meta property='twitter:image' content="<?php echo get_the_post_thumbnail_url(get_the_ID(),'square-crop'); ?>?nocache=true"/>
 	<meta name="twitter:card" content="summary_large_image"/>
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 
