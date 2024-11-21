8be8007c7926e1db3962dcf3eeac4581e86a068d
suisrael <88337104+suisrael@users.noreply.github.com>
2024-11-21 16:20:04 -0500

    revert to OOB twenty-seventeen search

--- header.php
+++ header.php
@@ -103,35 +103,7 @@
 
 </head>
 
-<body 
-
-
-
-<?php 
-
-$this_category = get_category($cat);
-//If category is parent, list it
-if ($this_category->category_parent == 0) {
-	$this_category->category_parent = $cat;
-} else { // If category is not parent, list parent category
-	$parent_category = get_category($this_category->category_parent);
-	//get the parent category id
-	$ParentCatId = $parent_category->cat_ID;
-	}
-	$childcategories = array();
-	$catx = $ParentCatId;
-	$cats = get_categories('parent='.$catx);
-	foreach($cats as $cat) {
-		$childcategories[] = $cat->term_id; } ;
-	//if it is a child category, add a class with the parent category id
-	if( is_category( $childcategories ) ) {
-		$class = 'parent-category-'.$ParentCatId;
-	}
-
-body_class($class); 
-
-
-?>>
+<body <?php body_class(); ?>>
 <!-- Google Tag Manager (noscript) -->
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=<?php echo $googleTagManager ?>"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
--- inc/search/readme.md
+++
@@ -1,4 +0,0 @@
-## remove
-
-- ~\template-parts\page\content-image-header.php (15)
-- ~\inc\custom-functions.php (18)
